1a05e365279a94862c0e45c1d838bedeff917cd0
dmitriy <dgavrichkov32@gmail.com>
2022-02-02 17:44:38 +0300

    delete old fetch code from action-creator

--- src/store/action-creators/tasks.ts
+++ src/store/action-creators/tasks.ts
@@ -6,25 +6,9 @@ import { TNewTask, TasksAction, TasksActionTypes, ITask } from "../../types/type
 
 
 // TODO
-// как-то это все неловко. Хорошо бы понять, как использовать текущее состояние getState и dispatch вместе
 export function toggleTaskAction(task: ITask) {
   return (dispatch: Dispatch<TasksAction>) => {
     try {
-      // const updatedTask = {...task, done: !task.done};
-      // fetch(`http://localhost:5000/tasks/${task.id}`, {
-      //   method: "PUT",
-      //   body: JSON.stringify(updatedTask),
-      //   headers: {
-      //     "Content-type": "application/json; charset=UTF-8",
-      //   }
-      // })
-      //   .then((response) => {
-      //     if(response.status === 200 && response.ok === true) {
-      //       dispatch({ type: TasksActionTypes.TOGGLE_TASK, payload: task.id });
-      //     }
-      //   })
-      //   .catch(e => console.log(e));
-
       dispatch({ type: TasksActionTypes.TOGGLE_TASK, payload: task.id });
 
     }
@@ -37,16 +21,7 @@ export function toggleTaskAction(task: ITask) {
 export function delTaskAction(id: string) {
   return (dispatch: Dispatch<TasksAction>) => {
     try {
-      // fetch(`http://localhost:5000/tasks/${id}`, {
-      //   method: "DELETE",
-      // })
-      //   .then((response) => {
-      //     if(response.status === 200 && response.ok === true) {
-      //       dispatch({ type: TasksActionTypes.DEL_TASK, payload: id });
-      //     }
-      //   })
       dispatch({ type: TasksActionTypes.DEL_TASK, payload: id });
-
     } 
     catch(e) {
       console.log(e);
@@ -58,21 +33,6 @@ export function addTaskAction(task: TNewTask) {
   const newTask = { id: nextId(), ...task, done: false };
   return (dispatch: Dispatch<TasksAction>) => {
     try {
-      // fetch("http://localhost:5000/tasks", {
-      //   method: "POST",
-      //   body: JSON.stringify(newTask),
-      //   headers: {
-      //     "Content-type": "application/json; charset=UTF-8",
-      //   }
-      // })
-      //   .then((response) => response.json())
-      //   .then((response) => {
-      //     dispatch({
-      //       type: TasksActionTypes.ADD_TASK,
-      //       payload: response
-      //     })
-      //   })
-      //   .catch((error) => console.log(error));
       dispatch({
         type: TasksActionTypes.ADD_TASK,
         payload: newTask
