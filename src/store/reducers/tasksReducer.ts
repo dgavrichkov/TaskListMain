7c33b2d50f9fb9d1f370c00c0b071bcdc222be7b
dmitriy <dgavrichkov32@gmail.com>
2022-02-10 13:46:25 +0300

    Удаление задач

--- src/store/reducers/tasksReducer.ts
+++ src/store/reducers/tasksReducer.ts
@@ -32,6 +32,9 @@ const data = (state = initialTasksState.data, action: TasksAction) => {
         ...state,
         [action.payload.id]: action.payload
       }
+    case TasksActionTypes.DEL_TASK:
+      const {[action.payload]: removedTask, ...restTasks} = state;
+      return restTasks;
     case TasksActionTypes.TOGGLE_TASK:
       const toggledTask = state[action.payload];
       return {
@@ -47,6 +50,8 @@ const idList = (state = initialTasksState.idList, action: TasksAction) => {
   switch(action.type) {
     case TasksActionTypes.ADD_TASK:
       return [...state, action.payload.id];
+    case TasksActionTypes.DEL_TASK:
+      return state.filter(task => task !== action.payload);
     default:
       return state
   }
@@ -56,37 +61,3 @@ export const tasksReducer = combineReducers({
   data,
   idList
 });
-
-
-// export const tasksReducer = (
-//   state = initialTasksState,
-//   action: TasksAction
-// ) => {
-//   switch (action.type) {
-//     // case TasksActionTypes.FETCH_TASKS:
-//     //   return { ...state };
-//     // case TasksActionTypes.FETCH_TASKS_SUCCESS:
-//     //   return { ...state, tasks: action.payload };
-//     // case TasksActionTypes.ADD_TASK:
-//     //   console.log(state);
-//     //   return {...state};
-//     // case TasksActionTypes.DEL_TASK:
-//     //   return {
-//     //     ...state,
-//     //     tasks: state.tasks.filter((task: ITask) => task.id !== action.payload)
-//     //   };
-//     // case TasksActionTypes.TOGGLE_TASK:
-//     //   const newTasks = state.tasks.map((task: ITask) => {
-//     //     if (task.id === action.payload) {
-//     //       task.done = !task.done;
-//     //     }
-//     //     return task;
-//     //   });
-//     //   return {
-//     //     ...state,
-//     //     tasks: newTasks
-//     //   };
-//     default:
-//       return state;
-//   }
-// };
