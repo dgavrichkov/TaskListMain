789353530eaf9ee096b922117e37e78535edf648
dmitriy_gavrichkov <dmitriy_gavrichkov@senla.eu>
2023-03-01 23:42:21 +0300

    update header

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto auto 1fr;
+  column-gap: 32px;
+
+  a {
+    color: ${(props) => props.theme.colors.text || `#000`};
+    &.is-active {
+      color: ${(props: any) => props.theme.colors.accent};
+    }
+  }
+`;
+
+const Nav = styled.nav`
+  display: grid;
+  gap: 14px;
+`;
+
+const SwitcherWrap = styled.div`
+  justify-self: end;
+`;
+
+export const Styled = { Header, Nav, SwitcherWrap };
