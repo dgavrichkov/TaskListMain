09f988d024a15466992c37fd577250d28e3a0d11
dmitriy_gavrichkov <dmitriy_gavrichkov@senla.eu>
2023-03-01 23:26:00 +0300

    add welcome page

---
+++ src/components/pages/Greeting.tsx
@@ -0,0 +1,7 @@
+export const Greeting = () => {
+  return (
+    <section>
+      <h2>Welcome to the task manager and notes keeper</h2>
+    </section>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import {App} from './App';
+import { App } from './App';
 import { store } from "./store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TasksPage } from "./components/pages/TasksPage";
 import { TaskPage } from './components/pages/TaskPage';
 import { NotesPage } from "./components/pages/NotesPage";
 import { NotePage } from './components/pages/NotePage';
+import { Greeting } from './components/pages/Greeting';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/TaskListMain" element={<App />}>
+          <Route index element={<Greeting />} />
           <Route path="tasks" element={<TasksPage />} />
           <Route path="notes" element={<NotesPage />} />
           <Route path="tasks/:taskId" element={<TaskPage />} />
