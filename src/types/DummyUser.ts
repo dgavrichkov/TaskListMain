84d36a5eab5d0343f52fb5c50064b2b70f384f01
dmitriy_gavrichkov <dmitriy_gavrichkov@senla.eu>
2023-03-07 15:23:13 +0300

    prepare user type

---
+++ src/types/DummyUser.ts
@@ -0,0 +1,23 @@
+export type TDummyUser = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  address: {
+    address: string;
+    city: string;
+    coordinates: {
+      lat: number;
+      lng: number;
+    };
+    postalCode: string;
+    state: string;
+  };
+};
