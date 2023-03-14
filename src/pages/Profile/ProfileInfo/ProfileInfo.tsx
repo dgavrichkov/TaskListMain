84cd041d1cc4d4c783443c865cc707cc7e06265a
dmitriy_gavrichkov <dmitriy_gavrichkov@senla.eu>
2023-03-14 14:01:18 +0300

    fix profile load on login

--- src/pages/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/pages/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,52 +3,57 @@ import { Styled } from './styled'
 
 export const ProfileInfo = () => {
   const { user } = useAuth();
+  console.log(user);
 
   return (
     <Styled.Wrap>
-      <Styled.InfoPanel>
-        <Styled.Entry>
-          <span>email</span>
-          <div>{user.email}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>phone</span>
-          <div>{user.phone}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>username</span>
-          <div>{user.username}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>password</span>
-          <div>{user.password}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>birthDate</span>
-          <div>{user.birthDate}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>address</span>
-          <div>{user.address.address}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>city</span>
-          <div>{user.address.city}</div>
-        </Styled.Entry>
-      </Styled.InfoPanel>
-      <Styled.PersonalPanel>
-        <div className='personal__image'>
-          <img width={100} height={100} src={user.image} alt="profile" />
-        </div>
-        <Styled.Entry>
-          <span>First name</span>
-          <div>{user.firstName}</div>
-        </Styled.Entry>
-        <Styled.Entry>
-          <span>Last name</span>
-          <div>{user.lastName}</div>
-        </Styled.Entry>
-      </Styled.PersonalPanel>
+      {user && (
+        <>
+          <Styled.InfoPanel>
+            <Styled.Entry>
+              <span>email</span>
+              <div>{user.email}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>phone</span>
+              <div>{user.phone}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>username</span>
+              <div>{user.username}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>password</span>
+              <div>{user.password}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>birthDate</span>
+              <div>{user.birthDate}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>address</span>
+              <div>{user.address?.address || 'none'}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>city</span>
+              <div>{user.address?.city || 'none'}</div>
+            </Styled.Entry>
+          </Styled.InfoPanel>
+          <Styled.PersonalPanel>
+            <div className='personal__image'>
+              <img width={100} height={100} src={user.image} alt="profile" />
+            </div>
+            <Styled.Entry>
+              <span>First name</span>
+              <div>{user.firstName}</div>
+            </Styled.Entry>
+            <Styled.Entry>
+              <span>Last name</span>
+              <div>{user.lastName}</div>
+            </Styled.Entry>
+          </Styled.PersonalPanel>
+        </>
+      )}
     </Styled.Wrap>
   )
 }
