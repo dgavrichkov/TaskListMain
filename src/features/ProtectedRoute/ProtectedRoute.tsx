64b56f2b23b1e6e9b504f927037e74739bab2e8e
dgavrichkov <dgavrichkov32@gmail.com>
2023-05-21 21:50:07 +0300

    change routing

--- src/features/ProtectedRoute/ProtectedRoute.tsx
+++ src/features/ProtectedRoute/ProtectedRoute.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 import { useAuth } from '../../app/providers/AuthProvider';
 
-export const ProtectedRoute: FC = ({ children }) => {
+export const ProtectedRoute: FC = () => {
   const { token } = useAuth();
 
   if (!token) {
     return <div>Forbidden, please log in</div>;
   }
 
-  return <>{children}</>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 };
