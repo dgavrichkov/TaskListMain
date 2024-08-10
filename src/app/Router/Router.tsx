ca32778f48ee0872703df014615fd1b97aef37c9
dgavrichkov <dgavrichkov32@gmail.com>
2024-08-10 16:06:16 +0300

    disable protected route

--- src/app/Router/Router.tsx
+++ src/app/Router/Router.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Greeting, Login, Note, Notes, Posts, Profile, Task, Tasks } from '../../pages';
 import { Examples } from '../../pages/Examples';
 import { PATHS } from '../../shared/constants/paths';
-import { ProtectedRoute } from '../../features';
+// import { ProtectedRoute } from '../../features';
 import { Layout } from '../Layout';
 
 export const Router: FC = () => (
@@ -11,14 +11,14 @@ export const Router: FC = () => (
     <Route element={<Layout />} path="/">
       <Route index element={<Greeting />} />
       <Route element={<Login />} path={PATHS.LOGIN} />
-      <Route element={<ProtectedRoute />}>
-        <Route element={<Tasks />} path={PATHS.TASKS} />
-        <Route element={<Posts />} path={PATHS.POSTS} />
-        <Route element={<Notes />} path={PATHS.NOTES} />
-        <Route element={<Task />} path={`${PATHS.TASKS}/:taskId`} />
-        <Route element={<Note />} path={`${PATHS.NOTES}/:noteId`} />
-        <Route element={<Profile />} path={PATHS.PROFILE} />
-      </Route>
+      {/* <Route element={<ProtectedRoute />}> */}
+      <Route element={<Tasks />} path={PATHS.TASKS} />
+      <Route element={<Posts />} path={PATHS.POSTS} />
+      <Route element={<Notes />} path={PATHS.NOTES} />
+      <Route element={<Task />} path={`${PATHS.TASKS}/:taskId`} />
+      <Route element={<Note />} path={`${PATHS.NOTES}/:noteId`} />
+      <Route element={<Profile />} path={PATHS.PROFILE} />
+      {/* </Route> */}
       <Route element={<Examples />} path={PATHS.EXAMPLES} />
     </Route>
   </Routes>
