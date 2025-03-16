df95135381f55f4023d1d127fe0219e0b9e97988
dgavrichkov <dgavrichkiv32@gmail.con>
2025-03-16 17:48:22 +0400

    add tabs, verbs admin form planned

--- src/pages/Verbs/Verbs.tsx
+++ src/pages/Verbs/Verbs.tsx
@@ -1,27 +1,20 @@
 import { useEffect, useState } from 'react';
 import cn from 'classnames';
-import { useAppDispatch, useAppSelector } from '../../app/store';
+import { useAppDispatch } from '../../app/store';
 import { verbModel } from '../../entities/verb';
+import { VerbAdmin } from './ui/VerbAdmin';
+import { VerbList } from './ui/VerbList';
 
 import styles from './Verbs.module.scss';
-import { VerbCard } from './ui/VerbCard';
+
+const TAB_MAP = {
+  verbs: <VerbList />,
+  admin: <VerbAdmin />,
+};
 
 export const Verbs = () => {
   const dispatch = useAppDispatch();
-  const wordsmap = useAppSelector(verbModel.selectors.selectWordsMap);
-  const words = useAppSelector(verbModel.selectors.selectWordsList);
-  const phrasals = useAppSelector(verbModel.selectors.selectPhrasals);
-
-  const [wordFilter, setWordFilter] = useState('');
-  const [phrasalFilter, setPhrasalFilter] = useState('');
-
-  const filteredWords = wordFilter
-    ? words.filter((word) => word.label.includes(wordFilter))
-    : words;
-
-  const filteredPhrasals = phrasalFilter
-    ? phrasals.filter((phrasal) => phrasal.words.includes(phrasalFilter))
-    : phrasals;
+  const [tabItem, setTabItem] = useState<'verbs' | 'admin'>('verbs');
 
   useEffect(() => {
     // TODO - разобраться с AnyAction
@@ -29,59 +22,23 @@ export const Verbs = () => {
     dispatch<any>(verbModel.actions.getPhrasalVerbsReference());
   }, [dispatch]);
 
-  const handleFilterWords = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    setWordFilter(event.target.value);
-  };
-
-  const handleFilterPhrasals = (wordId: string) => {
-    if (phrasalFilter === wordId) {
-      setPhrasalFilter('');
-      return;
-    } else {
-      setPhrasalFilter(wordId);
-    }
-  };
-
   return (
     <section className={styles.wrap}>
-      <h1 className={styles.title}>Verbs</h1>
-      <section className={styles.words}>
-        <h2>Words for phrasal verbs</h2>
-        <div>
-          <input placeholder="Search word" type="text" onChange={handleFilterWords} />
-        </div>
-        <div>
-          Active filter: <span>{wordsmap[phrasalFilter]?.label || 'Нет'}</span>
-        </div>
-        <ul className={styles.wordsList}>
-          {filteredWords.map((word) => (
-            <li className={styles.word} key={word.id}>
-              <button
-                className={cn(styles.wordButton, { [styles.active]: phrasalFilter === word.id })}
-                title={`id: ${word.id}`}
-                onClick={() => handleFilterPhrasals(word.id)}
-              >
-                {word.label}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section className={styles.phrasals}>
-        <h2>Phrasal verbs list</h2>
-        {filteredPhrasals && filteredPhrasals.length ? (
-          <ul className={styles.phrasalsList}>
-            {filteredPhrasals.map((phrasals) => (
-              <li key={phrasals.id}>
-                <VerbCard item={phrasals} wordsmap={wordsmap} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          'No phrasal verbs'
-        )}
-      </section>
+      <header>
+        <button
+          className={cn(styles.tabItem, { [styles.active]: tabItem === 'verbs' })}
+          onClick={() => setTabItem('verbs')}
+        >
+          Verbs
+        </button>
+        <button
+          className={cn(styles.tabItem, { [styles.active]: tabItem === 'admin' })}
+          onClick={() => setTabItem('admin')}
+        >
+          Admin
+        </button>
+      </header>
+      {TAB_MAP[tabItem]}
     </section>
   );
 };
---
+++ src/pages/Verbs/ui/VerbAdmin/VerbAdmin.tsx
@@ -0,0 +1,14 @@
+export const VerbAdmin = () => {
+  return (
+    <div>
+      <div>Verb Admin Form</div>
+      <ul>
+        <li>title</li>
+        <li>words array (pick from reference)</li>
+        <li>meaning (description/explanation)</li>
+        <li>ru translation</li>
+        <li>list of examples</li>
+      </ul>
+    </div>
+  );
+};
