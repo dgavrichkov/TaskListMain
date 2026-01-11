ca4984562000b73fb28618e7525ccc546fc50cb2
dgavrichkov <dgavrichkiv32@gmail.con>
2026-01-11 22:15:30 +0400

    wip - started to think of flashcards

---
+++ src/features/Flashcards/domain/flashcard.interfaces.ts
@@ -0,0 +1,5 @@
+export interface IFlashcard {
+  id: string;
+  frontDocumentId: string;
+  backDocumentId: string;
+}
---
+++ src/features/Flashcards/domain/use_case/createFlashcard.usecase.ts
@@ -0,0 +1,9 @@
+// Здесь будет что-то вроде этого
+
+// export async function createFlashcard(input: CreateFlashcardInput) {
+//   validate(input);
+//   const card = await repo.create(input);
+//   await analytics.track('flashcard_created');
+//
+//   return card;
+// }
